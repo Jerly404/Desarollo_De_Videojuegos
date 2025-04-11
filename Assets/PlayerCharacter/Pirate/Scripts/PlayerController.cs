c5264c594c74424f5c007e9b45fec46d30089c47
Jerly404 <hjerlycondorluna@gmail.com>
2025-04-11 15:39:14 -0500

    Adding movement to the right

---
+++ Assets/PlayerCharacter/Pirate/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocityX = 0;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.linearVelocityX = 5;
+        }
+        
+    }
+}
