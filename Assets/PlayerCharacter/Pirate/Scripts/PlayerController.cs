408d6ccf957eed866511f8caffda5e0e1ab6989b
Jerly404 <hjerlycondorluna@gmail.com>
2025-04-11 16:06:49 -0500

    Status: Run character animation

--- Assets/PlayerCharacter/Pirate/Scripts/PlayerController.cs
+++ Assets/PlayerCharacter/Pirate/Scripts/PlayerController.cs
@@ -4,26 +4,31 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     SpriteRenderer sr;
+    Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.linearVelocityX = 0;
+        animator.SetInteger("Estado", 0);
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.linearVelocityX = 5;
             sr.flipX = false;
+            animator.SetInteger("Estado", 1);
 
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.linearVelocityX = -5;
             sr.flipX = true;
+            animator.SetInteger("Estado", 1);
         }
         if (Input.GetKey(KeyCode.Space))
         {
