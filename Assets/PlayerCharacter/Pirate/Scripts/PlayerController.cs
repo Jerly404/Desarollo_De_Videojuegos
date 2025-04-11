d50f603a91efc260575dea6d7078103c71a39a74
Jerly404 <hjerlycondorluna@gmail.com>
2025-04-11 15:47:01 -0500

    Add character facing direction based on movement

--- Assets/PlayerCharacter/Pirate/Scripts/PlayerController.cs
+++ Assets/PlayerCharacter/Pirate/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
+    SpriteRenderer sr;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -15,10 +17,13 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             rb.linearVelocityX = 5;
+            sr.flipX = false;
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.linearVelocityX = -5;
+            sr.flipX = true;
         }
         
     }
